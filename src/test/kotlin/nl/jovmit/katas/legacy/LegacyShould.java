00998b347a110461f099e2ea3de715d035bea506
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-16 08:48:06 +0100

    Failing test for saving new weekly default card

--- src/test/kotlin/nl/jovmit/katas/legacy/LegacyShould.java
+++ src/test/kotlin/nl/jovmit/katas/legacy/LegacyShould.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import static nl.jovmit.katas.legacy.ActionsWeeklyReportDefaultCardBuilder.aWeeklyCard;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class LegacyShould {
@@ -31,5 +33,26 @@ public class LegacyShould {
         legacy.validateAndUpdateDefaultCard(userContext, configuredCardsInOrder);
 
         assertNull(repository.find(USER_ID, CARD_NAME));
+    }
+
+    @Test
+    public void save_new_weekly_default_card() {
+        ActionsWeeklyReportDefaultCard weeklyCard = aWeeklyCard()
+                .withCardName(FIRST_CARD_NAME)
+                .withUserId(USER_ID)
+                .build();
+
+        UserContext userContext = UserContextBuilder.aUserContext()
+                .withUserId(USER_ID)
+                .withFeatureEnabled()
+                .build();
+        CardType card1 = new CardType(FIRST_CARD_NAME);
+        CardType card2 = new CardType(SECOND_CARD_NAME);
+        List<CardType> configuredCardsInOrder = Arrays.asList(card1, card2);
+
+        Legacy legacy = new Legacy(repository);
+        legacy.validateAndUpdateDefaultCard(userContext, configuredCardsInOrder);
+
+        assertEquals(weeklyCard, repository.find(USER_ID, FIRST_CARD_NAME));
     }
 }
