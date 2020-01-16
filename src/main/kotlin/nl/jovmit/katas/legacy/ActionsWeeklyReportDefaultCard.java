ad4a85425fd5504dec4751f522682e6ed7a25a0b
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-16 07:51:36 +0100

    InMemeorydefaultCardRepository can store and look up cards

--- src/main/kotlin/nl/jovmit/katas/legacy/ActionsWeeklyReportDefaultCard.java
+++ src/main/kotlin/nl/jovmit/katas/legacy/ActionsWeeklyReportDefaultCard.java
@@ -3,8 +3,23 @@ package nl.jovmit.katas.legacy;
 import java.util.UUID;
 
 class ActionsWeeklyReportDefaultCard {
+
+    private final UUID userId;
+    private final String name;
+    private final int initialCount;
+
     public ActionsWeeklyReportDefaultCard(UUID userId, String name, int initialCount) {
-        throw new UnsupportedOperationException("Not Implemented");
+        this.userId = userId;
+        this.name = name;
+        this.initialCount = initialCount;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getNoTimesShown() {
--- src/test/kotlin/nl/jovmit/katas/legacy/InMemoryDefaultCardRepository.java
+++ src/test/kotlin/nl/jovmit/katas/legacy/InMemoryDefaultCardRepository.java
@@ -1,8 +1,12 @@
 package nl.jovmit.katas.legacy;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 class InMemoryDefaultCardRepository extends ActionsDefaultCardRepository {
+
+    private List<ActionsWeeklyReportDefaultCard> cards = new ArrayList<>();
 
     @Override
     public void deleteIfExists(UUID userId) {
@@ -11,12 +15,16 @@ class InMemoryDefaultCardRepository extends ActionsDefaultCardRepository {
 
     @Override
     public ActionsWeeklyReportDefaultCard find(UUID userId, String name) {
-        return super.find(userId, name);
+        return cards.stream()
+                .filter(it -> it.getUserId().equals(userId) &&
+                        it.getName().equals(name))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public void save(ActionsWeeklyReportDefaultCard actionsWeeklyReportDefaultCard) {
-        super.save(actionsWeeklyReportDefaultCard);
+        cards.add(actionsWeeklyReportDefaultCard);
     }
 
     @Override
