8e01958a87a8e1b62455869171827cdea5aa91b6
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-20 08:22:27 +0100

    Extracted a method for promoting new weekly card

--- src/main/kotlin/nl/jovmit/katas/legacy/Legacy.java
+++ src/main/kotlin/nl/jovmit/katas/legacy/Legacy.java
@@ -36,22 +36,25 @@ class Legacy {
         if (!weeklyDefaultCards.contains(configuredActionCardsInOrder.get(TOP_PRIO_INDEX).name())) {
             actionsDefaultCardRepository.deleteIfExists(userId);
         } else {
-            CardType defaultCardConfigured = configuredActionCardsInOrder.get(TOP_PRIO_INDEX);
+            promoteNewWeeklyCard(userContext, configuredActionCardsInOrder, userId);
+        }
+    }
 
-            //check the entry of the default card in the table, if there is no entry create one
-            ActionsWeeklyReportDefaultCard actionsWeeklyReportDefaultCard = actionsDefaultCardRepository.find(userId, defaultCardConfigured.name());
-            if (actionsWeeklyReportDefaultCard == null) {
-                promoteDefaultCardFor(userId, defaultCardConfigured);
-            } else if (actionsWeeklyReportDefaultCard.getNoTimesShown() == MAX_NO_TIMES_TO_SHOW) {
-                //check the count, if it is 2 delete the current one from db and also remove from the list
-                //and repeat the process, the scenario will not be repeated if the card in question is the MSM general card
-                //because that is the lowest in prio and there is no card after that
-                //But, in case the current card is SS and count is 2 already, it will be removed from the list, deleted from the table and the recursive
-                //call will make a new entry of the MSM card in the db and show it in the list
-                actionsDefaultCardRepository.delete(userId, actionsWeeklyReportDefaultCard.getCardType());
-                configuredActionCardsInOrder.remove(TOP_PRIO_INDEX);
-                validateAndUpdateDefaultCard(userContext, configuredActionCardsInOrder);
-            }
+    private void promoteNewWeeklyCard(UserContext userContext, List<CardType> configuredActionCardsInOrder, UUID userId) {
+        CardType defaultCardConfigured = configuredActionCardsInOrder.get(TOP_PRIO_INDEX);
+        //check the entry of the default card in the table, if there is no entry create one
+        ActionsWeeklyReportDefaultCard actionsWeeklyReportDefaultCard = actionsDefaultCardRepository.find(userId, defaultCardConfigured.name());
+        if (actionsWeeklyReportDefaultCard == null) {
+            promoteDefaultCardFor(userId, defaultCardConfigured);
+        } else if (actionsWeeklyReportDefaultCard.getNoTimesShown() == MAX_NO_TIMES_TO_SHOW) {
+            //check the count, if it is 2 delete the current one from db and also remove from the list
+            //and repeat the process, the scenario will not be repeated if the card in question is the MSM general card
+            //because that is the lowest in prio and there is no card after that
+            //But, in case the current card is SS and count is 2 already, it will be removed from the list, deleted from the table and the recursive
+            //call will make a new entry of the MSM card in the db and show it in the list
+            actionsDefaultCardRepository.delete(userId, actionsWeeklyReportDefaultCard.getCardType());
+            configuredActionCardsInOrder.remove(TOP_PRIO_INDEX);
+            validateAndUpdateDefaultCard(userContext, configuredActionCardsInOrder);
         }
     }
 
