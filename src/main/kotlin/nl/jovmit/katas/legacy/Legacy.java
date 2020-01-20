1fd4b34ed931050a3644a25d56faf87acb0c79df
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-20 08:25:41 +0100

    Improved naming

--- src/main/kotlin/nl/jovmit/katas/legacy/Legacy.java
+++ src/main/kotlin/nl/jovmit/katas/legacy/Legacy.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 class Legacy {
 
     private static final List<String> ACTIONS_WEEKLY_REPORT_DEFAULT_CARDS = Collections.emptyList();
-    private static final int TOP_PRIO_INDEX = 0;
+    private static final int TOP_PRIORITY_INDEX = 0;
     private static final int INITIAL_COUNT = 0;
     private static final int MAX_NO_TIMES_TO_SHOW = 0;
 
@@ -32,7 +32,7 @@ class Legacy {
 
     private void updateDefaultCard(UserContext userContext, List<CardType> configuredActionCardsInOrder) {
         final UUID userId = userContext.getUserId();
-        if (!weeklyDefaultCards.contains(configuredActionCardsInOrder.get(TOP_PRIO_INDEX).name())) {
+        if (!weeklyDefaultCards.contains(configuredActionCardsInOrder.get(TOP_PRIORITY_INDEX).name())) {
             actionsDefaultCardRepository.deleteIfExists(userId);
         } else {
             promoteNewWeeklyCard(userContext, configuredActionCardsInOrder, userId);
@@ -40,14 +40,14 @@ class Legacy {
     }
 
     private void promoteNewWeeklyCard(UserContext userContext, List<CardType> configuredActionCardsInOrder, UUID userId) {
-        CardType defaultCardConfigured = configuredActionCardsInOrder.get(TOP_PRIO_INDEX);
+        CardType defaultCardConfigured = configuredActionCardsInOrder.get(TOP_PRIORITY_INDEX);
         ActionsWeeklyReportDefaultCard actionsWeeklyReportDefaultCard =
                 actionsDefaultCardRepository.find(userId, defaultCardConfigured.name());
         if (actionsWeeklyReportDefaultCard == null) {
             promoteDefaultCardFor(userId, defaultCardConfigured);
         } else if (actionsWeeklyReportDefaultCard.getNoTimesShown() == MAX_NO_TIMES_TO_SHOW) {
             actionsDefaultCardRepository.delete(userId, actionsWeeklyReportDefaultCard.getCardType());
-            configuredActionCardsInOrder.remove(TOP_PRIO_INDEX);
+            configuredActionCardsInOrder.remove(TOP_PRIORITY_INDEX);
             validateAndUpdateDefaultCard(userContext, configuredActionCardsInOrder);
         }
     }
