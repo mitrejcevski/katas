8d8f6fce05dfaaa932ed41b94a300b4c8a47bb4d
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-20 08:23:38 +0100

    Removed not very useful comments from code

--- src/main/kotlin/nl/jovmit/katas/legacy/Legacy.java
+++ src/main/kotlin/nl/jovmit/katas/legacy/Legacy.java
@@ -32,7 +32,6 @@ class Legacy {
 
     private void updateDefaultCard(UserContext userContext, List<CardType> configuredActionCardsInOrder) {
         final UUID userId = userContext.getUserId();
-        //check if top prio card is one of the 2 default cards, if no then delete any entry in the table because it has to be 2 times in a row (reset)
         if (!weeklyDefaultCards.contains(configuredActionCardsInOrder.get(TOP_PRIO_INDEX).name())) {
             actionsDefaultCardRepository.deleteIfExists(userId);
         } else {
@@ -42,16 +41,10 @@ class Legacy {
 
     private void promoteNewWeeklyCard(UserContext userContext, List<CardType> configuredActionCardsInOrder, UUID userId) {
         CardType defaultCardConfigured = configuredActionCardsInOrder.get(TOP_PRIO_INDEX);
-        //check the entry of the default card in the table, if there is no entry create one
         ActionsWeeklyReportDefaultCard actionsWeeklyReportDefaultCard = actionsDefaultCardRepository.find(userId, defaultCardConfigured.name());
         if (actionsWeeklyReportDefaultCard == null) {
             promoteDefaultCardFor(userId, defaultCardConfigured);
         } else if (actionsWeeklyReportDefaultCard.getNoTimesShown() == MAX_NO_TIMES_TO_SHOW) {
-            //check the count, if it is 2 delete the current one from db and also remove from the list
-            //and repeat the process, the scenario will not be repeated if the card in question is the MSM general card
-            //because that is the lowest in prio and there is no card after that
-            //But, in case the current card is SS and count is 2 already, it will be removed from the list, deleted from the table and the recursive
-            //call will make a new entry of the MSM card in the db and show it in the list
             actionsDefaultCardRepository.delete(userId, actionsWeeklyReportDefaultCard.getCardType());
             configuredActionCardsInOrder.remove(TOP_PRIO_INDEX);
             validateAndUpdateDefaultCard(userContext, configuredActionCardsInOrder);
@@ -60,7 +53,7 @@ class Legacy {
 
     private void promoteDefaultCardFor(UUID userId, CardType defaultCardConfigured) {
         ActionsWeeklyReportDefaultCard actionsWeeklyReportDefaultCard;
-        actionsDefaultCardRepository.deleteIfExists(userId); //takes care of any other entry if present, so that we only have one entry per user at a time
+        actionsDefaultCardRepository.deleteIfExists(userId);
         actionsWeeklyReportDefaultCard = new ActionsWeeklyReportDefaultCard(userId, defaultCardConfigured.name(), INITIAL_COUNT);
         actionsDefaultCardRepository.save(actionsWeeklyReportDefaultCard);
     }
